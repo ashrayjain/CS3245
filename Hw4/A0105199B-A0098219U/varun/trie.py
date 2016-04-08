90da8c287f972f95c07f3c1434038568354b6325
Varun Patro <varun.kumar.patro@gmail.com>
2016-04-09 01:28:11 +0800

    add trie implementation

---
+++ Hw4/A0105199B-A0098219U/varun/trie.py
@@ -0,0 +1,37 @@
+import os
+import xml.etree.ElementTree as ET
+import pygtrie as trie
+
+ipc_trie = None
+
+def init(filepath):
+  global ipc_trie
+  IPC_Dict = dict()
+
+  files = os.listdir(filepath)
+
+  for file in files:
+    try:
+      doc = ET.parse(filepath + file).getroot()
+      IPCs = doc.find('str/[@name="All IPC"]').text.strip()
+      IPC_Raw_List = IPCs.split('|')
+      IPC_List = map(lambda x: x.replace('/', '').strip(), IPC_Raw_List)
+      for ipc in IPC_List:
+        if ipc not in IPC_Dict:
+          IPC_Dict[ipc] = set()
+        
+        IPC_Dict[ipc].add(file)
+    except AttributeError:
+      # All IPC field probably doesn't exist
+      # print file
+      pass
+
+  ipc_trie = trie.CharTrie(IPC_Dict)
+
+def getfiles(prefix):
+  if not ipc_trie:
+    raise LookupError("Call init first")
+
+  file_sets = ipc_trie.values(prefix)
+  file_sets_union = reduce(lambda x, y: x.union(y), file_sets)
+  return file_sets_union
