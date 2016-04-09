e01dd691be283a1cdc6dd4008751e5744ca7e846
Karan Kamath <karankamath26@gmail.com>
2016-04-09 18:41:06 +0800

    Add eval script

---
+++ Hw4/A0105199B-A0098219U/eval.py
@@ -0,0 +1,36 @@
+import sys
+
+fpos = sys.argv[1]
+fneg = sys.argv[2]
+fout = sys.argv[3]
+
+with open(fpos) as fp, open(fneg) as fn, open(fout) as fo:
+    ppres, fpres = ([], [])
+
+    def populate(f, arr):
+        for line in f:
+            line = line.strip()
+            if line:
+                arr.append(line)
+
+    pos = []
+    neg = []
+
+    populate(fp, pos)
+    populate(fn, neg)
+
+    output = []
+    for line in fo:
+        output.extend(line.strip().split())
+
+    for o in output:
+        if o in pos:
+            ppres.append(o)
+        if o in neg:
+            fpres.append(o)
+
+    print "Positive Accuracy:", str( round(len(ppres) * 100.0 / len(pos), 2) )
+    print "Negative Accuracy:", str( 100 - round(len(fpres) * 100.0 / len(neg), 2) )
+ 
+    print "Missing:", str([x for x in pos if x not in ppres])
+    print "Extra:", str([x for x in fpres if x in neg])
