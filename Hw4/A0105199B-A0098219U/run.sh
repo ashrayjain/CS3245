b72f0e411ab7e9b909403a736e9b2eb3be901e19
Karan Kamath <karankamath26@gmail.com>
2016-04-15 00:05:35 +0800

    Revert to old engine. similar fail

--- Hw4/A0105199B-A0098219U/run.sh
+++ Hw4/A0105199B-A0098219U/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 rm -f dictionary.txt postings.txt
-python index.py -i patsnap-corpus/ -d dictionary.txt -p postings.txt --debug
+python index.py -i patsnap-corpus/ -d dictionary.txt -p postings.txt #--debug
--- Hw4/A0105199B-A0098219U/search.py
+++ Hw4/A0105199B-A0098219U/search.py
@@ -1,8 +1,10 @@
 import argparse
+from nltk import Text
+from nltk.corpus import PlaintextCorpusReader
+from nltk.tokenize.treebank import TreebankWordTokenizer
 from utils import preprocess_text
 import xml.etree.ElementTree as et
-from search_engine import IPCEngine
-
+from search_engine import Engine
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--dictionary', required=True,
@@ -19,18 +21,44 @@ args = parser.parse_args()
 
 to_strip = "Relevant documents will describe "
 
+def get_thesaurus():
+    corpus = PlaintextCorpusReader(
+                './processed_corpus/', 
+                '.*', 
+                word_tokenizer=TreebankWordTokenizer())
+    
+    thesaurus = Text(word.lower() for word in corpus.words())
+    return thesaurus
+
+def expand(query, thesaurus):
+    additional_terms = ""
+
+    for w in query:
+        similar_terms = thesaurus.similar(w)
+        if similar_terms:
+            additional_terms += ' ' + similar_terms
+
+    query += additional_terms
+
+    return query
+
 with open(args.queries, 'r') as fq:
+    thesaurus = get_thesaurus()
+
     text = fq.read()
     root = et.fromstring(text)
     t = ""
     for child in root:
         text = child.text.replace(to_strip, "")
         t += "\n" + text.strip()
+
+    expanded_query = expand(t, thesaurus)
+        
     query_map = preprocess_text(t)
 
 
 with open(args.output, 'w') as fo:
-    engine = IPCEngine()
+    engine = Engine(args.dictionary, args.postings)
     result = engine.execute_query(query_map)
     if result is None:
         fo.write('\n')
